a78f4b4b5b314eae8c2ea24dc2aa01865dce4d66
Julie-ann <julieanndingalorcine@.com>
2023-01-28 15:38:46 +0800

    first commit

---
+++ resources/js/Pages/Tasks/Create.vue
@@ -0,0 +1,116 @@
+<template>
+    <AppLayout title="Create Task">
+        <div class="flex px-4 m-10">
+            
+        </div>
+    <FormSection >
+        <template #title>
+            Profile Information
+        </template>
+
+        <template #description>
+            Update your account's profile information and email address.
+        </template>
+
+        <template #form>
+
+            <!-- Description -->
+            <div class="col-span-6 sm:col-span-4">
+                <InputLabel for="description" value="Description" />
+                <TextInput
+                    id="description"
+                    v-model="form.description"
+                    type="text"
+                    class="mt-1 block w-full"
+                    autocomplete="description"
+                />
+                <InputError :message="form.errors.description" class="mt-2" />
+            </div>
+
+            <!-- Status -->
+            <div class="col-span-6 sm:col-span-4">
+                <InputLabel for="status" value="Status" />
+                <TextInput
+                    id="status"
+                    v-model="form.status"
+                    type="text"
+                    class="mt-1 block w-full"
+                    autocomplete="status"
+                />
+                <InputError :message="form.errors.status" class="mt-2" />
+            </div>
+
+            <!-- Assignee -->
+            <div class="col-span-6 sm:col-span-4">
+                <InputLabel for="assignee" value="Assignee" />
+                <TextInput
+                    id="assignee"
+                    v-model="form.assignee"
+                    type="text"
+                    class="mt-1 block w-full"
+                    autocomplete="assignee"
+                />
+                <InputError :message="form.errors.assignee" class="mt-2" />
+            </div>
+            
+        </template>
+
+        <template #actions>
+            <ActionMessage :on="form.recentlySuccessful" class="mr-3">
+                Saved.
+            </ActionMessage>
+
+            <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click="saveTask">
+                Save
+            </PrimaryButton>
+        </template>
+    </FormSection>
+    </AppLayout>
+</template>
+<script>
+ import AppLayout from "@/Layouts/AppLayout.vue";
+ import { Link } from '@inertiajs/vue3';
+ import ActionMessage from '@/Components/ActionMessage.vue';
+import FormSection from '@/Components/FormSection.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+
+ export default{
+    components: {
+        AppLayout,
+        Link,
+        ActionMessage,
+        FormSection,
+        InputError,
+        InputLabel,
+        PrimaryButton,
+        SecondaryButton,
+        TextInput
+        
+    },
+
+    data:function() {       //diri ang variables
+        return{
+            form: this.$inertia.form({
+                description:null,
+                status:null,
+                assignee:null
+            }),
+                
+        };
+    },
+
+    methods: {
+        saveTask() {
+            this.$inertia.post(route('tasks.store'), this.form,{
+                onSuccess:() => {
+                this.form.reset();
+            }
+            });
+        }
+    }
+ }
+</script>
---
+++ resources/js/Pages/Tasks/Index.vue
@@ -0,0 +1,57 @@
+<template>
+    <AppLayout title="Task Management">
+        <template #header>
+            <PrimaryButton class="float-right" @click="createTask">
+                Create task
+            </PrimaryButton>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Task Management
+            </h2>
+        </template>
+    
+        <div class="flex px-4 m-10">
+            <table class ="border-separate border-spacing-3 border border-slate-500">
+                <thead>
+                    <tr>
+                        <th >ID</th>
+                        <th >Description</th>
+                        <th >Status</th>
+                        <th >Assignee</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(task,index) in collection.tasks" :key="index">
+                        <td >{{ task.id }}</td>
+                        <td >{{ task.description }}</td>
+                        <td >{{ task.status }}</td>
+                        <td >{{ task.assignee }}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    </AppLayout>
+</template>
+<script>
+ import AppLayout from "@/Layouts/AppLayout.vue";
+ import PrimaryButton from '@/Components/PrimaryButton.vue';
+ import { router } from '@inertiajs/vue3'
+
+ export default{
+
+    props: {
+        collection:Object
+    },
+
+    components: {
+        AppLayout,
+        PrimaryButton
+    },
+    methods: {
+        createTask() {
+            this.$inertia.visit(route('tasks.create'), { method: 'get' })
+        }
+    }
+
+ }
+</script>
